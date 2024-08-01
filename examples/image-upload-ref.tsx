e6e26d0c864202f5f7a86ead4dc0ec343ca55207
AyoungWon <wayinwayne@gmail.com>
2024-08-01 18:30:37 +0900

    feat: examples 추가

---
+++ examples/image-upload-ref.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import useImageMetadata from "../src/hooks/useImageMetadata"; //배포하고 path 바꾸기
+
+const ImageUploadRefSample = () => {
+  const { ref, imageMetadata } = useImageMetadata({
+    validateOptions: {
+      width: 1000,
+      height: { max: 2000, onError: (error) => console.log(error) },
+    },
+  });
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <input ref={ref} type="file" accept=".jpg, .jpeg" />
+      {imageMetadata && (
+        <img
+          style={{ width: imageMetadata.width, height: imageMetadata.height }}
+          src={imageMetadata.src}
+          alt="image file"
+        />
+      )}
+    </div>
+  );
+};
+
+export default ImageUploadRefSample;
---
+++ examples/image-upload.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ImageUploader from "../src/components/image-uploader"; //배포하고 path 바꾸기
+import { useState } from "react";
+import { type ImageFileMetadata } from "../src/hooks/useImageMetadata"; //배포하고 path 바꾸기
+
+const ImageUploadSample = () => {
+  const [imageMetadata, setMetadata] = useState<ImageFileMetadata>();
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <ImageUploader
+        accept=".jpg, .jpeg"
+        onMetadataLoaded={(data) => {
+          setMetadata(data);
+        }}
+        validateOptions={{
+          width: {
+            max: 3000,
+            onError: (error) => {
+              window.alert(
+                `${error.field}의 ${error.max}을 초과하였습니다. 선택된 파일의 ${error.field}:${error.selectedFileValue}`
+              );
+            },
+          },
+          height: 3000,
+        }}
+      >
+        <button>
+          {imageMetadata ? "reselect Image File" : "Select Image File"}
+        </button>
+      </ImageUploader>
+      {imageMetadata && (
+        <img
+          style={{ width: imageMetadata.width, height: imageMetadata.height }}
+          src={imageMetadata.src}
+          alt="image file"
+        />
+      )}
+    </div>
+  );
+};
+
+export default ImageUploadSample;
