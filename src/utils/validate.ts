4a5a7dce1d2a8af5f8375cae96692391a25e276d
AyoungWon <wayinwayne@gmail.com>
2024-08-01 17:49:23 +0900

    refac: validate utils내 함수 가독성 향상을 위한 리팩토링

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -15,58 +15,71 @@ export interface ValidateOptions {
   size?: NumberOption;
 }
 
+const handleError = ({
+  errorHandler,
+  field,
+  value,
+  max,
+  unit,
+}: {
+  errorHandler?: ErrorHandler;
+  field: string;
+  value: number;
+  max: number;
+  unit?: string;
+}) => {
+  const errorParams: ValidateError = { field, selectedFileValue: value, max };
+  if (errorHandler) errorHandler(errorParams);
+  else
+    window.alert(
+      `이미지 파일의 ${field}는 ${max}${unit ?? ""}보다 작거나 같아야합니다.`
+    );
+};
+
 const validateOption = (
   field: string,
   value: number,
-  condition: number | { max: number; onError?: ErrorHandler }
+  condition: NumberOption,
+  unit?: string
 ) => {
-  if (typeof condition === "number") {
-    if (value > condition) {
-      window.alert(
-        `이미지 파일의 ${field}는 ${condition}보다 작거나 같아야합니다.`
-      );
-      return false;
-    }
-  } else {
-    if (value > condition.max) {
-      if (condition.onError)
-        condition.onError({
-          field,
-          selectedFileValue: condition.max,
-          max: value,
-        });
-      else
-        window.alert(
-          `이미지 파일의 ${field}는 ${condition.max}보다 작거나 같아야합니다.`
-        );
-      return false;
-    }
+  const max = typeof condition === "number" ? condition : condition.max;
+
+  if (value > max) {
+    const errorHandler =
+      typeof condition === "object" ? condition.onError : undefined;
+    handleError({ errorHandler, max, value, field, unit });
+    return false;
   }
+
   return true;
 };
 export const validateImageFile = (
   option: ValidateOptions,
   metaData: ImageFileMetadata
 ) => {
-  if (option.width) {
-    const validatePassed = validateOption(
-      "width",
-      metaData.width,
-      option.width
-    );
-    if (!validatePassed) return false;
-  }
-  if (option.height) {
-    const validatePassed = validateOption(
-      "height",
-      metaData.height,
-      option.height
-    );
-    if (!validatePassed) return false;
-  }
-  if (option.size) {
-    const validatePassed = validateOption("size", metaData.size, option.size);
-    if (!validatePassed) return false;
-  }
-  return true;
+  const validations: {
+    field: keyof ImageFileMetadata;
+    value: number;
+    condition: NumberOption | undefined;
+    unit?: string;
+  }[] = [
+    {
+      field: "width",
+      value: metaData.width,
+      condition: option.width,
+      unit: "px",
+    },
+    {
+      field: "height",
+      value: metaData.height,
+      condition: option.height,
+      unit: "px",
+    },
+    { field: "size", value: metaData.size, condition: option.size, unit: "kb" },
+  ];
+
+  return validations.every(
+    ({ field, value, condition, unit }) =>
+      condition === undefined || validateOption(field, value, condition, unit)
+  );
 };
