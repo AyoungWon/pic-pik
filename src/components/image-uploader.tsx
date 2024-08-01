c3f1cdbad79f1943bef5283de5e5d51aab051dda
AyoungWon <wayinwayne@gmail.com>
2024-08-01 15:42:00 +0900

    feat: image 파일선택시 metadata 만드는 로직useImageMetadata으로 분리

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -1,49 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import React, { ChangeEvent, useState } from "react";
+import React, { useEffect } from "react";
+import useImageMetadata from "../hooks/useImageMetadata";
 
+//TODO
 //label의 스타일 받기
 //input에 들어갈 props 받기
 //children 받아서 label에 넣어주기
+//imageMetadata 로드에 대한 콜백 받기
 interface ImageUploaderProps {}
 
-export interface ImageFileMetadata {
-  height: number;
-  width: number;
-  size: number;
-  name: string;
-  extension: string;
-}
-
 const ImageUploader: React.FC<ImageUploaderProps> = ({}) => {
-  const [imageMetadata, setImageMetadata] = useState<ImageFileMetadata | null>(
-    null
-  );
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const target = e.target as FileReader;
-
-      const image = new Image();
-      image.src = target.result as string;
-      image.onload = () => {
-        const { height, width } = image;
-        const totalSizeKB = Math.ceil(e.total / 1024);
-        const fileName = event.target.files?.[0].name ?? "";
-        const fileExtension = (fileName.split(".").pop() ?? "").toLowerCase();
-        setImageMetadata({
-          height,
-          width,
-          size: totalSizeKB,
-          name: fileName,
-          extension: fileExtension,
-        });
-      };
-    };
-    const target = event.target as HTMLInputElement;
-
-    if (target.files && target.files[0]) reader.readAsDataURL(target.files[0]);
-  };
+  const { ref, imageMetadata } = useImageMetadata();
+  useEffect(() => {
+    console.log(imageMetadata);
+  }, [imageMetadata]);
 
   return (
     <label htmlFor={"pic_pik_file_input"}>
@@ -53,7 +24,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({}) => {
         type="file"
         accept="image/*"
         css={_hidden}
-        onChange={handleFileChange}
+        ref={ref}
       />
     </label>
   );
---
+++ src/hooks/useImageMetadata.ts
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+
+interface Props {}
+
+export interface ImageFileMetadata {
+  height: number;
+  width: number;
+  size: number;
+  name: string;
+  extension: string;
+  src: string;
+}
+
+const useImageMetadata = () => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const [imageMetadata, setImageMetadata] = useState<ImageFileMetadata | null>(
+    null
+  );
+  const handleFileChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const readerTarget = e.target as FileReader;
+      const image = new Image();
+      image.src = readerTarget.result as string;
+      image.onload = () => {
+        const { height, width } = image;
+        const totalSizeKB = Math.ceil(e.total / 1024);
+        const fileName = target.files?.[0].name ?? "";
+        const fileExtension = (fileName.split(".").pop() ?? "").toLowerCase();
+        setImageMetadata({
+          height,
+          width,
+          size: totalSizeKB,
+          name: fileName,
+          extension: fileExtension,
+          src: image.src,
+        });
+      };
+    };
+
+    if (target.files && target.files[0]) reader.readAsDataURL(target.files[0]);
+  };
+
+  useEffect(() => {
+    const fileInput = ref.current;
+    if (fileInput && !fileInput.dataset.listenerAdded) {
+      // 이벤트 리스너 등록
+      fileInput.addEventListener("change", handleFileChange);
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      return () => {
+        fileInput.removeEventListener("change", handleFileChange);
+      };
+    }
+  }, [ref.current]);
+
+  return { ref, imageMetadata };
+};
+
+export default useImageMetadata;
