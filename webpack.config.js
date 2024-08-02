7bde54ae22bafc84dca2fe7d86d92f93f21a5966
AyoungWon <wayinwayne@gmail.com>
2024-08-02 14:17:18 +0900

    fix: webpack build error 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
+// __dirname과 __filename은 ES 모듈에서 직접 사용 불가능하므로, 아래와 같이 정의합니다.
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   mode: "development",
   entry: "./src/index.tsx",
   output: {
@@ -38,5 +43,4 @@ module.exports = {
     port: 8080,
     open: true,
   },
-  // externals 제거
 };
