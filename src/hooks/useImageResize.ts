cef83d27e6b37882a2f9c7314f584af562bea90a
AyoungWon <wayinwayne@gmail.com>
2024-08-08 16:16:29 +0900

    feat: resizeImage 함수 테스트를 위한 useImageResize의 properties 이름 및 타입 변경

--- src/hooks/useImageResize.ts
+++ src/hooks/useImageResize.ts
@@ -1,36 +1,36 @@
 import { useEffect, useState } from "react";
 
-interface ImageResizeProps {
-  blob: Blob;
-}
-
-interface AspectRatioWidth extends ImageResizeProps {
+interface AspectRatioWidth {
   mode: "aspectRatio";
   width: number;
 }
 
-interface AspectRatioHeight extends ImageResizeProps {
+interface AspectRatioHeight {
   mode: "aspectRatio";
   height: number;
 }
 
-interface AspectRatioScale extends ImageResizeProps {
+interface AspectRatioScale {
   mode: "aspectRatio";
   scale: number;
 }
 
-interface StretchResize extends ImageResizeProps {
+interface StretchResize {
   mode: "stretch";
   width?: number; // width와 height 중 하나는 반드시 있어야 함
   height?: number;
 }
 
-type ResizeProps =
+type ResizeOption =
   | AspectRatioWidth
   | AspectRatioHeight
   | AspectRatioScale
   | StretchResize;
 
+interface Props {
+  blob?: Blob;
+  option: ResizeOption;
+}
 interface ResizeDimensions {
   targetWidth: number;
   targetHeight: number;
@@ -39,30 +39,30 @@ interface ResizeDimensions {
 const calculateResizeDimensions = (
   imgWidth: number,
   imgHeight: number,
-  props: ResizeProps
+  option: ResizeOption
 ): ResizeDimensions => {
   let targetWidth: number = imgWidth; // 초기값 설정
   let targetHeight: number = imgHeight; // 초기값 설정
 
-  const { mode } = props;
+  const { mode } = option;
 
   switch (mode) {
     case "aspectRatio":
-      if ("width" in props) {
-        targetWidth = props.width;
+      if ("width" in option) {
+        targetWidth = option.width;
         targetHeight = (imgHeight / imgWidth) * targetWidth;
-      } else if ("height" in props) {
-        targetHeight = props.height;
+      } else if ("height" in option) {
+        targetHeight = option.height;
         targetWidth = (imgWidth / imgHeight) * targetHeight;
-      } else if ("scale" in props) {
-        targetWidth = imgWidth * props.scale;
-        targetHeight = imgHeight * props.scale;
+      } else if ("scale" in option) {
+        targetWidth = imgWidth * option.scale;
+        targetHeight = imgHeight * option.scale;
       }
       break;
 
     case "stretch":
-      targetWidth = props.width || imgWidth;
-      targetHeight = props.height || imgHeight;
+      targetWidth = option.width || imgWidth;
+      targetHeight = option.height || imgHeight;
       break;
 
     default:
@@ -73,45 +73,47 @@ const calculateResizeDimensions = (
   return { targetWidth, targetHeight };
 };
 
-const useImageResize = (props: ResizeProps) => {
+export const resizeImage = async (
+  blob: Blob,
+  option: ResizeOption,
+  onComplete: (blob: Blob) => void
+) => {
+  const img = new Image();
+  img.src = URL.createObjectURL(blob);
+
+  img.onload = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    if (!ctx) return;
+
+    const { targetHeight, targetWidth } = calculateResizeDimensions(
+      img.width,
+      img.height,
+      option
+    );
+
+    canvas.width = targetWidth;
+    canvas.height = targetHeight;
+
+    // 이미지 리사이즈 및 캔버스에 그리기
+    ctx.drawImage(img, 0, 0, targetWidth, targetHeight);
+
+    // 캔버스에서 Blob으로 변환
+    canvas.toBlob((resizedBlob) => {
+      if (resizedBlob) {
+        onComplete(resizedBlob);
+      }
+    }, blob.type);
+  };
+};
+
+const useImageResize = ({ blob, option }: Props) => {
   const [resizedBlob, setResizedBlob] = useState<Blob | null>(null);
 
-  const resizeImage = async () => {
-    const { blob } = props;
-
-    const img = new Image();
-    img.src = URL.createObjectURL(blob);
-
-    img.onload = () => {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-
-      if (!ctx) return;
-
-      const { targetHeight, targetWidth } = calculateResizeDimensions(
-        img.width,
-        img.height,
-        props
-      );
-
-      canvas.width = targetWidth;
-      canvas.height = targetHeight;
-
-      // 이미지 리사이즈 및 캔버스에 그리기
-      ctx.drawImage(img, 0, 0, targetWidth, targetHeight);
-
-      // 캔버스에서 Blob으로 변환
-      canvas.toBlob((resizedBlob) => {
-        if (resizedBlob) {
-          setResizedBlob(resizedBlob);
-        }
-      }, blob.type);
-    };
-  };
-
   useEffect(() => {
-    resizeImage();
-  }, [props]);
+    if (blob) resizeImage(blob, option, setResizedBlob);
+  }, [blob, option]);
 
   return resizedBlob;
 };
