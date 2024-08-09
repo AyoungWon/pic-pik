b519edfda778361a1bbbcc97fe46248a87ae0c61
AyoungWon <wayinwayne@gmail.com>
2024-08-09 23:09:10 +0900

    feat: useResizeImage 관련 examples 파일 추가

---
+++ examples/resize-image/src/app.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useImage, useResizeImage } from 'pic-pik"';
+
+const App = () => {
+  const { ref, metadata: imageOriginalMetadata } = useImage();
+  const { metadata } = useResizeImage({
+    metadata: imageOriginalMetadata,
+    option: { mode: "aspectRatio", scale: 0.2 },
+  });
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <input type="file" ref={ref} />
+      {imageOriginalMetadata && (
+        <img
+          src={imageOriginalMetadata.src}
+          width={imageOriginalMetadata.width}
+        />
+      )}
+      {metadata && <img src={metadata.src} width={metadata.width} />}
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,5 @@
-import { createRoot } from "react-dom/client";
 import ImageLoader from "./components/image-loader";
 import useImage from "./hooks/useImage";
 import useResizeImage from "./hooks/useResizeImage";
-
-// const App = () => {
-//   return <></>;
-// };
-
-// const container = document.getElementById("root");
-// if (container) {
-//   const root = createRoot(container);
-//   root.render(<App />);
-// }
 
 export { ImageLoader, useImage, useResizeImage };
