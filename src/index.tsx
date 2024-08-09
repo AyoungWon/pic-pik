b8806bae873eb00340d9148e69a3d8b0196291d4
AyoungWon <wayinwayne@gmail.com>
2024-08-09 22:45:05 +0900

    test: resize image 테스트 코드 초안 작성

--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,14 @@ import ImageLoader from "./components/image-loader";
 import useImage from "./hooks/useImage";
 import useResizeImage from "./hooks/useResizeImage";
 
-const App = () => {
-  const { ref, metadata: imageOriginalMetadata } = useImage();
-  const { metadata } = useResizeImage({
-    metadata: imageOriginalMetadata,
-    option: { mode: "aspectRatio", scale: 0.2 },
-  });
+// const App = () => {
+//   return <></>;
+// };
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <input type="file" ref={ref} />
-      {imageOriginalMetadata && (
-        <img
-          src={imageOriginalMetadata.src}
-          width={imageOriginalMetadata.width}
-        />
-      )}
-      {metadata && <img src={metadata.src} width={metadata.width} />}
-    </div>
-  );
-};
+// const container = document.getElementById("root");
+// if (container) {
+//   const root = createRoot(container);
+//   root.render(<App />);
+// }
 
-const container = document.getElementById("root");
-if (container) {
-  const root = createRoot(container);
-  root.render(<App />);
-}
-
-export { ImageLoader, useImage };
+export { ImageLoader, useImage, useResizeImage };
