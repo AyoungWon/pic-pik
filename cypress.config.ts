66560ae40401e9ca3de44f7b9f972bf54c0a89c9
AyoungWon <wayinwayne@gmail.com>
2024-08-13 17:16:42 +0900

    chore: 테스트 커버리지를 위한 istanbul 세팅

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,23 @@
+// cypress.config.js 또는 cypress.config.ts
 import { defineConfig } from "cypress";
+import codeCoverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config); // 코드 커버리지 태스크 추가
+      return config;
+    },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
   component: {
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config); // 코드 커버리지 태스크 추가
+      return config;
+    },
     devServer: {
       framework: "react",
       bundler: "vite",
-      viteConfig: {}, // 여기에 Vite 설정을 추가할 수 있습니다.
     },
-    supportFile: "cypress/support/component.ts",
   },
 });
